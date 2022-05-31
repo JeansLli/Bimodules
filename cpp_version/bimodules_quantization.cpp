888d5bac11e073c97cc5623e842956de8d95d581
Jingyi Li <jingyili@JingyideMacBook-Pro.local>
2022-05-31 23:19:18 +0800

    optimized cpp version

--- cpp_version/bimodules_quantization.cpp
+++ cpp_version/bimodules_quantization.cpp
@@ -13,6 +13,7 @@
  
 using Simplex_tree = Gudhi::Simplex_tree<>;
 using Vertex_handle = Simplex_tree::Vertex_handle;
+using Simplex_handle =  Simplex_tree::Simplex_handle;
 using Filtration_value = Simplex_tree::Filtration_value;
 using typeVectorVertex = std::vector<Vertex_handle>;
 using typePairSimplexBool = std::pair<Simplex_tree::Simplex_handle, bool>;
@@ -20,7 +21,10 @@ using Field_Zp = Gudhi::persistent_cohomology::Field_Zp;
 using Persistent_cohomology = Gudhi::persistent_cohomology::Persistent_cohomology<Simplex_tree, Field_Zp >;
 
 typedef std::vector<std::vector<std::vector<std::vector<int> > > > rank; 
-
+typedef std::pair<int,int> entry;
+typedef std::pair<entry,entry> bar;
+typedef std::map<bar, int> barcode;
+typedef std::map<Simplex_handle,entry> simplex_grid_map;
 
 using std::cout; 
 using std::cerr;
@@ -39,7 +43,7 @@ struct simplex_node{
 vector<string> split(string &str, const string &pattern)
 {
     char * strc = new char[strlen(str.c_str())+1];
-    strcpy(strc, str.c_str());   //string转换成C-string
+    strcpy(strc, str.c_str());   
     vector<string> res;
     char* temp = strtok(strc, pattern.c_str());
     while(temp != NULL)
@@ -51,87 +55,80 @@ vector<string> split(string &str, const string &pattern)
     return res;
 }
 
-vector<int> create_stair(vector<int> elbow, vector<int> end_point){
-	vector<int> stair;
+vector<entry> create_stair(entry elbow, entry end_point){
+	vector<entry> stair;
 	int i,j,k,l;
 	
-	if(elbow[0]==0 || elbow[1]==end_point[1]){
+	if(elbow.first==0 || elbow.second==end_point.second){
 
-		for(i=0; i<=end_point[1]; ++i){
-			stair.push_back(0);
-			stair.push_back(i);
+		for(i=0; i<=end_point.second; ++i){
+            stair.push_back(entry(0,i));
 		}
 		--i;
-		for(j=1; j<=end_point[0]; ++j){
-			stair.push_back(j);
-			stair.push_back(i);
+		for(j=1; j<=end_point.first; ++j){
+            stair.push_back(entry(j,i));
 		}
 	}
     else{
-    	for(i=0; i<=elbow[1]; ++i){
-			stair.push_back(0);
-			stair.push_back(i);
+    	for(i=0; i<=elbow.second; ++i){
+            stair.push_back(entry(0,i));
 		}
 		--i;
-		for(j=0; j<elbow[0]; ++j){
-			stair.push_back(j+1);
-			stair.push_back(i);
+		for(j=0; j<elbow.first; ++j){
+            stair.push_back(entry(j+1,i));
 		}
 		--j;
-		for(k=i+1; k<=end_point[1]; ++k){
-			stair.push_back(j+1);
-			stair.push_back(k);
+		for(k=i+1; k<=end_point.second; ++k){
+            stair.push_back(entry(j+1,k));
 		}
 		--k;
-		for(l=j+2; l<=end_point[0]; ++l){
-			stair.push_back(l);
-			stair.push_back(k);
+		for(l=j+2; l<=end_point.first; ++l){
+            stair.push_back(entry(l,k));
 		}
     }
     return stair;
 }
 
 
-vector<int> proj_grid_to_path(simplex_node simplex, vector<int> elbow, int x_range,int y_range){
-	int x = simplex.grid_x;
-	int y = simplex.grid_y;
-	int x0 = elbow[0];
-    int y0 = elbow[1];
-    vector<int> proj_p;
+entry proj_grid_to_path(entry simplex_node, entry elbow, int x_range,int y_range){
+	int x = simplex_node.first;
+	int y = simplex_node.second;
+	int x0 = elbow.first;
+    int y0 = elbow.second;
+    entry proj_p;
 
     if((x==0 && y<=y0) || (y==y0 && x<=x0) || (y>=y0 && x==x0) || (y==y_range && x>=x0)){
-        proj_p.push_back(x);
-    	proj_p.push_back(y);
+        proj_p.first = x;
+        proj_p.second = y;
     }
     else if(x<=x0 and y<=y0){
-    	proj_p.push_back(x);
-    	proj_p.push_back(y0);
+        proj_p.first = x;
+        proj_p.second = y0;
     }
     else if(x<=x0 and y>=y0){
-    	proj_p.push_back(x0);
-    	proj_p.push_back(y);
+        proj_p.first = x0;
+        proj_p.second = y;
     }
     else{
-    	proj_p.push_back(x);
-    	proj_p.push_back(y_range);
+        proj_p.first = x;
+        proj_p.second = y_range;
     }
-
 	return proj_p;
 }
 
 
 void zero_rank (rank& r, int n, int m) {  // n columns, m rows in the grid (column-dominant)
-  r.resize(n);
-  for (std::vector<std::vector<std::vector<int> > >& u: r){
-    u.resize(m);
-    for (std::vector<std::vector<int> >& v: u){
-      v.resize(n);
-      for (std::vector<int>& w: v) {
-    w.resize(m);
-    w.assign(m, 0);
-      }
+    r.resize(n);
+    for (std::vector<std::vector<std::vector<int> > >& u: r){
+        u.resize(m);
+        for (std::vector<std::vector<int> >& v: u){
+            v.resize(n);
+            for (std::vector<int>& w: v) {
+                w.resize(m);
+                w.assign(m, 0);
+            }
+        }
     }
-  }
 }
 
 int main(int argc, char* const argv[]) {
@@ -139,22 +136,19 @@ int main(int argc, char* const argv[]) {
 	int y_range=20;
 	int line_i = 0;
     int vertice_id = 0;
-    int rank_dim = 0;
+    int rank_dim = 1;
     vector<int> rank_num;
     vector<float> x_values;
     vector<float> y_values;
     vector<simplex_node> simplices;
     vector<string> block_lines;
 
-    vector<int> end_point;
-    end_point.push_back(x_range);
-    end_point.push_back(y_range);
-
-
+    entry end_point(x_range,y_range);
+    simplex_grid_map sg_map;
 
     //Read file
-	//string filename("../datasets/fig3-2.txt");
-	string filename("../../cgta_paper_2021/function_rips/function_rips_with_threshold_1000000_1.scc");
+	string filename("../../../cgta_paper_2021/function_rips/function_rips_with_threshold_1000000_1.scc");
+    //string filename("../../data/fig3-2.txt");
 	//vector<string> lines;
     string line;
 
@@ -218,6 +212,7 @@ int main(int argc, char* const argv[]) {
     	start_id += rank_num[i];
     }
 
+    Simplex_tree st;
 
     start_id = rank_num[rank_num.size()-2] + rank_num[rank_num.size()-1];
     int start_id_last;
@@ -242,6 +237,8 @@ int main(int argc, char* const argv[]) {
     	}
     	start_id += rank_num[i];
     }
+
+    
 /*
   	//print the simplices  
     for(int i=0; i<simplices.size(); ++i){
@@ -266,6 +263,7 @@ int main(int argc, char* const argv[]) {
     float y_interval = (y_max-y_min)/y_range;
 
     vector<simplex_node> simplices_integer=simplices;
+    //cout << "sg_map.size()="<<sg_map.size()<<endl;
 
     for(int i=0; i<simplices.size();++i){
     	float grid_x = simplices[i].grid_x;
@@ -283,7 +281,33 @@ int main(int argc, char* const argv[]) {
     		simplices_integer[i].grid_y = round((grid_y-y_min)/y_interval);
     	}
 
+        //initialize the simplex tree and map
+        std::pair<Simplex_handle, bool> insert_return = st.insert_simplex(simplices_integer[i].face, Filtration_value(i));//initialize the simplex tree
+        if(insert_return.second==false){
+            cout << "ERROR:insert fail!!!" << endl;
+            return 0;
+        }
+
     }
+
+    //std::cout << "The complex contains " << st.num_simplices() << " simplices - " << st.num_vertices() << " vertices "<< std::endl;
+    int i=0;
+    for (auto f_simplex : st.filtration_simplex_range()) {
+        sg_map[f_simplex] = entry(simplices_integer[i].grid_x,simplices_integer[i].grid_y);
+        //std::cout << "   " << "[" << st.filtration(f_simplex) << "] ";
+        //for (auto vertex : st.simplex_vertex_range(f_simplex)) {
+        //    std::cout << static_cast<int>(vertex) << " -- ";
+        //}
+        //std::cout << ";" << std::endl;
+        ++i;
+    }
+
+    /*
+    cout<<"check the value of sg_map "<<endl;
+    for ( const auto &p : sg_map ){
+        std::cout  << p.second.first << " " << p.second.second << std::endl;
+    } 
+    */
 
 
 /*
@@ -300,49 +324,115 @@ int main(int argc, char* const argv[]) {
     }
 */
 
-    Simplex_tree st;
+
+    
     rank rank_inv_tmp,rank_inv;
     zero_rank(rank_inv,x_range+1,y_range+1);
-    
+
 
     for(int i=0; i<=x_range; ++i){
     	for(int j=0; j<=y_range; ++j){
     		cout << "elbow: " << i << " " << j << endl;
-    		vector<int> elbow;
-    		elbow.push_back(i);
-    		elbow.push_back(j);
+    		entry elbow(i,j);
 
-    		vector<int> stair = create_stair(elbow, end_point);
-    		//for(int stair_id=0;stair_id<stair.size();++stair_id)
-    		//	cout << stair[stair_id] << " ";
-    		//cout<<""<<endl;
+    		vector<entry> stair = create_stair(elbow, end_point);
 
     		clock_t start_time=clock();
+            /*
     		for(int k=0; k<simplices_integer.size();++k){
     			simplex_node simplex = simplices_integer[k];
-    			vector<int> proj_p = proj_grid_to_path(simplex, elbow, x_range, y_range);
-    			float filtration_value=proj_p[0]+proj_p[1];
+    			entry proj_p = proj_grid_to_path(simplex, elbow, x_range, y_range);
+    			float filtration_value = proj_p.first+proj_p.second;
     			if(st.find(simplex.face)==st.null_simplex()){
     				st.insert_simplex(simplex.face, Filtration_value(filtration_value));
     			}
     		}
+            */
+            
+            for(Simplex_handle sh: st.complex_simplex_range()){
+                entry node_grid = sg_map[sh];
+                //for(Vertex_handle vh: sh.Simplex_vertex_iterator()){
+                //    cout<<vh<<endl;
+                //}
+                //cout << "node_grid="<<node_grid.first<<","<< node_grid.second<<endl;
+                //cout << "node_grid="<<sg_map[sh].first<<","<< sg_map[sh].second<<endl;
+                entry proj_p = proj_grid_to_path(node_grid, elbow, x_range, y_range);
+                //cout << "proj_p="<<proj_p.first<<","<< proj_p.second<<endl;
+                st.assign_filtration(sh,proj_p.first+proj_p.second);
+            }
+            
+            st.initialize_filtration();//will sort the simplex by filtration order
+            //print the filtration
+            /*
+            for (auto f_simplex : st.filtration_simplex_range()) {
+                std::cout << "   " << "[" << st.filtration(f_simplex) << "] ";
+                for (auto vertex : st.simplex_vertex_range(f_simplex)) {
+                    std::cout << static_cast<int>(vertex) << " -- ";
+                }
+                std::cout << ";" << std::endl;
+            
+            }*/
+
+
+
+            
+
     		clock_t end_time=clock();
     		cout << "time of building a simplex tree "<<(double)(end_time-start_time)/CLOCKS_PER_SEC <<"s"<<endl;
 
   			start_time=clock();
-  			int coeff_field_characteristic=2;
+  			int coeff_field_characteristic=11;
   			int min_persistence=0;
   			Persistent_cohomology pcoh(st);
-
   			pcoh.init_coefficients(coeff_field_characteristic);
- 
   			pcoh.compute_persistent_cohomology(min_persistence);
-  			end_time=clock();
 
+            //TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            // Don't use get_persistent_pairs()
+            // Use intervals_in_dimension() :https://gudhi.inria.fr/doc/3.1.1/class_gudhi_1_1persistent__cohomology_1_1_persistent__cohomology.html#a05b896bcbac713daeefb03644d9ed006
+            // Compare persistent function of C++ version and python version
+            auto persistent_pairs = pcoh.get_persistent_pairs();
+
+            zero_rank(rank_inv_tmp,x_range+1,y_range+1);
+            //pcoh.output_diagram();
+
+            //cout<<"========"<<endl;
+
+            
+            for (auto pair : persistent_pairs) {
+                //cout << st.dimension(get<0>(pair)) << " "<< st.filtration(get<0>(pair)) << " "<< st.filtration(get<1>(pair)) << endl;
+                if(st.dimension(get<0>(pair))==rank_dim){
+                    int barcode_sid = st.filtration(get<0>(pair));
+                    int barcode_eid;
+                    if(st.filtration(get<0>(pair))>x_range+y_range+2){
+                        barcode_eid = x_range+y_range+1;
+                    }else{
+                        barcode_eid = st.filtration(get<1>(pair));
+                    }
+                    //cout << "barcode_sid="<<barcode_sid<<endl;
+                    //cout << "barcode_eid="<<barcode_eid<<endl;
+                    for(int r_s=barcode_sid; r_s<barcode_eid; ++r_s){
+                        for(int r_e=r_s; r_e<barcode_eid; ++r_e){
+                            int ii,jj,kk,ll;
+                            ii = stair[r_s].first;
+                            jj = stair[r_s].second;
+                            kk = stair[r_e].first;
+                            ll = stair[r_e].second;
+                            //cout << "rank_inv_location="<<ii << " "<<jj << " "<< kk << " "<<ll<<endl;
+                            //cout<< "rank_inv_tmp[]="<<rank_inv_tmp[ii][jj][kk][ll]<<endl;
+                            rank_inv_tmp[ii][jj][kk][ll] += 1;
+                        }
+                    }
+
+                }
+            }
+
+  			end_time=clock();
   			cout <<"time of calculating barcodes "<<(double)(end_time-start_time)/CLOCKS_PER_SEC <<"s"<<endl;
  
   			// Output the diagram in filediag
   			//pcoh.output_diagram();
+            /*
   			string output_file = "../datasets/barcodes_tmp";
   			std::ofstream out(output_file);
     		pcoh.output_diagram(out);
@@ -371,13 +461,15 @@ int main(int argc, char* const argv[]) {
     						//cout <<"start "<<r_s<<endl;
     						//cout <<"end " <<r_e<<endl;
     						//cout << "rank_coor" << stair[r_s*2] << " "<<stair[r_s*2+1] << " "<<stair[r_e*2] << " "<<stair[r_e*2+1] << endl;
-    						rank_inv_tmp[stair[r_s*2]][stair[r_s*2+1]][stair[r_e*2]][stair[r_e*2+1]] +=1;	
+    						//rank_inv_tmp[stair[r_s*2]][stair[r_s*2+1]][stair[r_e*2]][stair[r_e*2+1]] +=1;	
+                            rank_inv_tmp[stair[r_s].first][stair[r_s].second][stair[r_e].first][stair[r_e].second] += 1;
     					}
     				}
     			}
     		}
+            */
 
-
+            
     		for(int s_x=0;s_x<=x_range;++s_x){
     			for(int s_y=0;s_y<=y_range;++s_y){
     				for(int t_x=s_x; t_x<=x_range; ++t_x){
@@ -389,12 +481,35 @@ int main(int argc, char* const argv[]) {
     				}
     			}
     		}
+            
+            /*
+            std::cout << "The complex contains " << st.num_simplices() << " simplices - " << st.num_vertices() << " vertices "<< std::endl;
+            for (auto f_simplex : st.filtration_simplex_range()) {
+                std::cout << "   " << "[" << st.filtration(f_simplex) << "] ";
+                for (auto vertex : st.simplex_vertex_range(f_simplex)) {
+                    std::cout << static_cast<int>(vertex) << " -- ";
+                }
+                std::cout << ";" << std::endl;
+            }
+            */
+            
 
     		end_time=clock();
-    		in_file.close();
-    		cout << "time of calculating rank invariance "<<(double)(end_time-start_time)/CLOCKS_PER_SEC <<"s"<<endl;
+    		//in_file.close();
+    		cout << "time of calculating rank invariant "<<(double)(end_time-start_time)/CLOCKS_PER_SEC <<"s"<<endl;
  		}
     }
+    /*
+    for(int i=0;i<=x_range;++i){
+        for(int j=0;j<=y_range;++j){
+            for(int k=0;k<=x_range;++k){
+                for(int l=0;l<=y_range;++l)
+                    cout<<"rank[("<<i<<","<<j<<"),("<<k<<","<<l<<")]="<<rank_inv[i][j][k][l]<<endl;
+
+            }
+        }
+    }*/
+    
 }
 
 
---
+++ cpp_version/build/CMakeFiles/bimodules_quantization.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bimodules_quantization.
+# This may be replaced when dependencies are built.
