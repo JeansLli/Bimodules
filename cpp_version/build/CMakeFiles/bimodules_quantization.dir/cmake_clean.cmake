7fef474eeab0ec6097c4f16e9d5da9bb15194f77
Jingyi Li <jingyili@jingyidembp.home>
2022-11-03 18:41:10 +0100

    minor modification

---
+++ cpp_version/build/CMakeFiles/bimodules_quantization.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bimodules_quantization.dir/bimodules_quantization.cpp.o"
+  "CMakeFiles/bimodules_quantization.dir/bimodules_quantization.cpp.o.d"
+  "bimodules_quantization"
+  "bimodules_quantization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bimodules_quantization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp_version/build/CMakeFiles/bimodules_quantization.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for bimodules_quantization.
+# This may be replaced when dependencies are built.
